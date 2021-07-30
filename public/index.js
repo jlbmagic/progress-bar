9ce2c0337c6763d2e60fe959782b6c677154d45e
Jeremy Brown <jeremy@geistinteractive.com>
2021-07-29 22:11:01 -0400

    first set up

--- public/index.js
+++ public/index.js
@@ -1,11 +1,22 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
 import "regenerator-runtime/runtime.js";
+var ProgressBar = require("progressbar.js");
+window.loadBar = function onLoad(json) {
+  const obj = JSON.parse(json);
+  const v = obj.value || 100;
+  const dur = obj.duration || 1000;
+  const per = v / 100;
+  var circle = new ProgressBar.Circle("#container", {
+    color: "#FCB03C",
+    duration: dur,
+    strokeWidth: 10,
+    easing: "easeInOut",
+    trailWidth: 25,
+    text: {
+      value: 100,
+    },
+  });
 
-
-const setText = () => {
-  document.getElementById("text").innerHTML =
-    "We are beginning! Oh Yes, we are!";
+  circle.animate(per);
 };
-
-document.getElementById("click").addEventListener("click", setText);
